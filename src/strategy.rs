9a928a8e1850a2873b37c0a97fc2b30d0886cacb
KTF41_gewei <grey@pku.edu.cn>
2025-06-06 17:05:40 +0800

    add later api

--- src/strategy.rs
+++ src/strategy.rs
@@ -7,4 +7,6 @@ pub trait Strategy: Send + Sync {
 
     /// Given a TickData, produce a new Order.
     fn update(&mut self, tick: &TickData) -> Order;
+    // fn update(&mut self, tick: &TickData);
+    // fn init_broker(&mut self, ctx: &zmq::Context, order_uri: &str, commission_fee: f64, margin_ratio: f64);
 }
